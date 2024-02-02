1fbfab3f38fcb099920a7b2d2fe88ecaa500a730
Sohaeb Ahmed <sohaebahmed@gmail.com>
2024-02-02 16:01:56 +0530

    u

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -2,25 +2,28 @@ import React from "react"
 import { IoReorderThreeOtline } from "react-icons/io5"
 const Sidebar = () => {
     return (
-        <div>
-            <div>
+        <div className="sticky top-0 h-[100vh]">
+            <div className="flex flex-col justify-between h-full">
                 <div>
-                    <img className='w-40' src="https://i.imgur.com/zqpwkLQ.png" alt="" />
-                </div>
-                <div className='mt-10'>
-                    {mainu.map((item) => <div className='flex item-center mb-5 cursor-pointer text-lg'>
-                        <p>{item.title}</p>
-                        {icon}
 
-                        </div>
-                    )} 
-                    
+                    <div className="pt-10">
+                        <img className='w-40' src="https://i.imgur.com/zqpwkLQ.png" alt="" />
+                    </div>
+                    <div className='mt-10'>
+                        {mainu.map((item) => <div className='flex item-center mb-5 cursor-pointer text-lg'>
+                            {itemicon}
+                            <p>{item.title}</p>
+                            </div>
+                        )} 
+                        
+                    </div>
+                </div>
+                <div className='flex items-center cursor-pointer'>
+                    <IoReorderThreeOtline />
+                    <p className='ml-5'>More</p>
                 </div>
             </div>
-            <div className='flex items-center cursor-pointer'>
-                <IoReorderThreeOtline />
-                <p className='ml-5'>More</p>
-            </div>
+            
         </div>
     )
 }
--- src/Components/Sidebar/SidebarConfig.js
+++ src/Components/Sidebar/SidebarConfig.js
@@ -2,35 +2,35 @@ import { AiOutlineHome } from "react-icons/ai";
 
 export const mainu = [
     {
-        title: "Home", icon: <AiOutlineHome />, iactiveicon: <AiFillHome></AiFillHome>
+        title: "Home", icon: <AiOutlineHome className="text-2xl mr-5" />, iactiveicon: <AiFillHome></AiFillHome>
     },
     {
-        title: "Search", icon:<AiOutlineSearch></AiOutlineSearch>, activeIcon:<AiOutlineSearch></AiOutlineSearch>
+        title: "Search", icon:<AiOutlineSearch className="text-2xl mr-5"></AiOutlineSearch>, activeIcon:<AiOutlineSearch></AiOutlineSearch>
     },
     {
-        title: "Explorer", icon: <AiOutlineCompass></AiOutlineCompass>, activeIcon: <AiFillCompass></AiFillCompass>
+        title: "Explorer", icon: <AiOutlineCompass className="text-2xl mr-5"></AiOutlineCompass>, activeIcon: <AiFillCompass></AiFillCompass>
     },
     {
-        title: "Reels", icon: <RiVideoLine></RiVideoLine>, activeIcon: <RiVideoFill></RiVideoFill>
+        title: "Reels", icon: <RiVideoLine className="text-2xl mr-5"></RiVideoLine>, activeIcon: <RiVideoFill></RiVideoFill>
     },
     {
         title: "Message",
-        icon: <AiOutlineMessage></AiOutlineMessage>,
+        icon: <AiOutlineMessage className="text-2xl mr-5"></AiOutlineMessage>,
         activeIcon: <AiFillMessage></AiFillMessage>
     },
     {
         title: "Notificatiion",
-        icon: <AiOutlineHeart></AiOutlineHeart>,
+        icon: <AiOutlineHeart className="text-2xl mr-5"></AiOutlineHeart>,
         activeIcon: <AiFillHeart></AiFillHeart>
     }, 
     {
         title: "Create",
-        icon: <AiOutlinePlusCircle></AiOutlinePlusCircle>,
+        icon: <AiOutlinePlusCircle className="text-2xl mr-5"></AiOutlinePlusCircle>,
         activeIcon: <AiFillPlusCircle></AiFillPlusCircle>
     }, 
     {
         title: "Profile",
-        icon: <CgProfile></CgProfile>,
+        icon: <CgProfile className="text-2xl mr-5"></CgProfile>,
         activeIcon: <CgProfile></CgProfile>
     }
 ]
