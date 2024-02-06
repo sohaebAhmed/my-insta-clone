d828137dc7a6ffae15498e001fbfc2c568a50c11
Sohaeb Ahmed <sohaebahmed@gmail.com>
2024-02-06 15:17:11 +0530

    m

--- src/Components/Post/PostCard.jsx
+++ src/Components/Post/PostCard.jsx
@@ -1,15 +1,25 @@
 import React from 'react'
 
 const PostCard = () => {
+    const [showDropDown, setShowDropDown] = useState(false)
+    const handleClick = () => {
+        setShowDropDown(!setShowDropDown)
+    }
     return (
         <div>
-            <div>
-                <div>
+            <div className='border rounded-md w-full'>
+                <div className='flex justify-between items-center w-full py-4 px-5'>
                     <div className='flex items-center'>
                         <img className='h-12 w-12 rounded-full' src="" alt="" />
                         <div className='pl-2'>
                             <p className='font-semibold text-sm'>username</p>
                             <p className='font-thin text-sm'>location</p>
+                        </div>
+                    </div>
+                    <div>
+                        <BsThreeDots/>
+                        <div onClick={handleClick} className='dropdown-content'>
+                            <p className='b--black text-white py-1 px-4 rounded-md cursor-pointer'>Delete</p>
                         </div>
                     </div>
                 </div>
