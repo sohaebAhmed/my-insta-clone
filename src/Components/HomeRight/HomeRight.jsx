927d9b258a25044db8e6ebe1efaf77d7b0e51e26
Sohaeb Ahmed <sohaebahmed@gmail.com>
2024-02-07 16:32:57 +0530

    m

--- src/Components/HomeRight/HomeRight.jsx
+++ src/Components/HomeRight/HomeRight.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import SuggestionCard from './SuggestionCard'
 
 const HomeRight = () => {
     return (
-        <div className='border'>
-            
+        <div className=''>
+            <div>
+                <div className='flex justify-between items-center'>
+                    <div className='flex items-center'>
+                        <div>
+                            <img className='w-12 h-12 rounded-full' src="" alt="" />
+                        </div>
+                        <div className='ml-3'>
+                            <p>fullname</p>
+                            <p className='opacity-70'>username</p>
+                        </div>
+                    </div>
+                    <div>
+                        <p className='text-blue-700 font-semibold'>switch</p>
+                    </div>
+                </div>
+                <div className='space-y-5 mt-10'>
+                    {[1, 1, 1, 1].map((item) => <SuggestionCard />)}
+                </div>
+            </div>
         </div>
     )
 }
