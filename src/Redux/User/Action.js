4a7c7ddcb336ed01d8c77806bc0e9471136df07e
Sohaeb Ahmed <sohaebahmed@gmail.com>
2024-02-15 15:32:50 +0530

    m

--- src/Redux/User/Action.js
+++ src/Redux/User/Action.js
@@ -1,4 +1,6 @@
-import { REQ_USER } from "./ActionType"
+import { FOLLOW_USER, GET_USERS_BY_USER_IDS, GET_USER_BY_USERNAME, REQ_USER, SEARCH_USER, UNFOLLOW_USER, UPDATE_USER } from "./ActionType"
+
+const BASE_API = "http://localhost:5454/api"
 
 export const getUserProfileAction = (jwt) => async (dispatch) => {
 
@@ -16,5 +18,98 @@ export const getUserProfileAction = (jwt) => async (dispatch) => {
 
     } catch (error) {
         console.log("catch :", error)
+    }
+}
+
+export const findUserByUserNameAction = (data) => async (dispatch) => {
+    const res = await fetch(`${BASE_API}/users/username/${data.username}`, {
+        method: "GET",
+        headers:{
+            "Content-Type" : "application/json",
+            Authorization: "Bearer " + data.jwt
+        }
+    })
+
+    const user = await res.json()
+    console.log("find by username: ", user)
+    dispatch({type:GET_USER_BY_USERNAME, payload:user})
+}
+
+export const findUserByUserUserIdsAction = (data) => async (dispatch) => {
+    const res = await fetch(`${BASE_API}/users/m/${data.userIds}`, {
+        method: "GET",
+        headers:{
+            "Content-Type" : "application/json",
+            Authorization: "Bearer " + data.jwt
+        }
+    })
+
+    const users = await res.json()
+    console.log("find by user ids : ", users)
+    dispatch({type:GET_USERS_BY_USER_IDS, payload:users})
+}
+
+export const followUserAction = (data) => async (dispatch) => {
+    const res = await fetch(`${BASE_API}/users/follow/${data.userId}`, {
+        method: "PUT",
+        headers:{
+            "Content-Type" : "application/json",
+            Authorization: "Bearer " + data.jwt
+        }
+    })
+    
+    const user = await res.json()
+    console.log("follow user: ", user)
+    dispatch({type:FOLLOW_USER, payload:user})
+}
+
+export const unFollowUserAction = (data) => async (dispatch) => {
+    const res = await fetch(`${BASE_API}/users/unfollow/${data.userId}`, {
+        method: "PUT",
+        headers:{
+            "Content-Type" : "application/json",
+            Authorization: "Bearer " + data.jwt
+        }
+    })
+
+    const user = await res.json()
+    console.log("unfollow user: ", user)
+    dispatch({type:UNFOLLOW_USER, payload:user})
+}
+
+export const searchUserAction = (data) => async (dispatch) => {
+    try {
+        const res = await fetch(`${BASE_API}/users/search?q=/${data.query}`, {
+            method: "GET",
+            headers:{
+                "Content-Type" : "application/json",
+                Authorization: "Bearer " + data.jwt
+            }
+        })
+    
+        const user = await res.json()
+        console.log("search user: ", user)
+        dispatch({type:SEARCH_USER, payload:user})
+    } catch (error) {
+        console.log("catch error : ", error)        
+    }
+}
+
+export const editUserAction = (data) => async (dispatch) => {
+    try {
+        const res = await fetch(`${BASE_API}/users/account/edit`, {
+            method: "PUT",
+            headers:{
+                "Content-Type" : "application/json",
+                Authorization: "Bearer " + data.jwt
+            },
+            body: JSON.stringify(data.data)
+        })
+    
+        const user = await res.json()
+        console.log("search user: ", user)
+        dispatch({type:UPDATE_USER, payload:user})
+    } catch (error) {
+        console.log("catch error : ", error)        
     }
 }
