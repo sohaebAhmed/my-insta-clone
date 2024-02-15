8231d98498270f001aba4b1283c928f1b466c5ad
Sohaeb Ahmed <sohaebahmed@gmail.com>
2024-02-15 15:43:37 +0530

    Update Reducer.js

--- src/Redux/User/Reducer.js
+++ src/Redux/User/Reducer.js
@@ -1,12 +1,30 @@
-import { REQ_USER } from "./ActionType"
+import { FOLLOW_USER, GET_USERS_BY_USER_IDS, GET_USER_BY_USERNAME, REQ_USER, SEARCH_USER, UNFOLLOW_USER, UPDATE_USER } from "./ActionType"
 
 const initialValue = {
     reqUser: null,
+    findByUsername: null,
+    findUserByIds: [],
+    followUser: null,
+    unfollowUser: null,
+    searchUser: null,
+    updatedUser: null
 }
 export const UserReducer = (store = initialValue, { type, payload }) => {
     if (type === REQ_USER) {
         return { ...store, reqUser: payload }
-    }
+    } else if (type === GET_USER_BY_USERNAME) {
+        return {...store, findByUsername:payload}
+    } else if (type === GET_USERS_BY_USER_IDS) {
+        return {...store, findUserByIds:payload}
+    } else if (type === FOLLOW_USER) {
+        return {...store, followUser:payload}
+    } else if (type === UNFOLLOW_USER) {
+        return {...store, unfollowUser:payload}
+    } else if (type === SEARCH_USER) {
+        return {...store, searchUser:payload}
+    } else if (type === UPDATE_USER) {
+        return {...store, updatedUser:payload}
+    } 
 
     return store
 }
