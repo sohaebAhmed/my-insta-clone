b92b1c9d23f03c5f3e991a3cdd43a87ac7dc32b8
Sohaeb Ahmed <sohaebahmed@gmail.com>
2024-03-14 14:02:34 +0530

    Updated Search Card

--- src/Components/SearchComponents/SearchComponents.jsx
+++ src/Components/SearchComponents/SearchComponents.jsx
@@ -1,28 +1,44 @@
-import React from 'react'
-import "./SearchComponents.css"
-import SearchUserCard from './SearchUserCard'
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { debounce } from "../../Config/Debounce";
+import { searchUserAction } from "../../Redux/User/Action";
+import "./SearchComponent.css";
+import SearchUserCard from "./SearchUserCard";
 
-const SearchComponents = () => {
-    const dispatch = useDispatch()
-    const token = localStorage.getItem("token")
-    const { user } = useSelector(store => store)
+const SearchComponent = ({ setIsSearchVisible }) => {
+    const token = localStorage.getItem("token");
+    const { user } = useSelector(store => store);
+    const dispatch = useDispatch();
+    const handleSearchUser = (query) => {
+        const data = {
+            jwt: token,
+            query,
+        };
+        dispatch(searchUserAction(data));
+    };
+    const debouncedHandleSearchUser = debounce(handleSearchUser, 1000);
 
-    dispatch(searchUserAction())
-    const handleSearch = (e) => {
-        dispatch(searchUserAction({ jwt: token, query: e.target.value }))
-    }
     return (
-        <div className='searchContainer'>
-            <div className='px-3 pb-5'>
-                <h1>Search</h1>
-                <input onChange={handleSearch} className='searchInput' type="text" placeholder='Search...' />
+        <div className="search-container">
+            <div className="px-3 pb-5">
+                <h1 className="text-xl pb-5">Search</h1>
+
+                <input
+                    onChange={(e) => debouncedHandleSearchUser(e.target.value)}
+                    className="search-input"
+                    type="text"
+                    placeholder="Search..."
+                />
             </div>
+
             <hr />
-            <div className='px-3 pt-5'>
-                {user.searchUser?.map((item) => <SearchUserCard user={item} />)}
+            <div className="px-3 pt-5">
+                {user.searchResult.map((item) => (
+                    <SearchUserCard setIsSearchVisible={setIsSearchVisible} key={item.id} username={item.username} image={item?.image} />
+                ))}
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default SearchComponents
+export default SearchComponent
